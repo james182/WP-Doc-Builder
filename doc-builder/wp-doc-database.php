edc6a0850a57a2ed6ca04f13f43d9183f500c728
James Lawrie <jameslawrie82@gmail.com>
2016-04-01 17:15:46 +1000

    Added files via upload

--- doc-builder/wp-doc-database.php
+++ doc-builder/wp-doc-database.php
@@ -1,3 +1,4 @@
+<?php
 
 /**
 * CREATE DATABASES
@@ -43,3 +44,24 @@ function jal_install() {
 	add_option( 'jal_db_version', $jal_db_version );
 }
 register_activation_hook( __FILE__, 'jal_install' );
+
+/*
+function jal_install_data() {
+	global $wpdb;
+	
+	$welcome_name = 'Mr. WordPress';
+	$welcome_text = 'Congratulations, you just completed the installation!';
+	
+	$table_name = $wpdb->prefix . 'doc_builder_elements';
+	
+	$wpdb->insert( 
+		$table_name, 
+		array( 
+			'time' => current_time( 'mysql' ), 
+			'name' => $welcome_name, 
+			'text' => $welcome_text, 
+		) 
+	);
+}
+register_activation_hook( __FILE__, 'jal_install_data' );
+*/
